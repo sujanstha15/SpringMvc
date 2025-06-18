2930196067faea69c4ac72d02ddc4bfde5078ec5
Sujan Shrestha <raju40rr@gmail.com>
2025-06-18 12:11:08 -0500

    Learned mapping and connecting between Controller class and HTML

--- dynamic-website/src/main/java/com/substring/dynamic/website/controllers/PageController.java
+++ dynamic-website/src/main/java/com/substring/dynamic/website/controllers/PageController.java
@@ -1,4 +1,33 @@
 package com.substring.dynamic.website.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/page")
 public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        //logic can be written here
+        System.out.println("Home controller method executed");
+
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String name = "Sujan";
+        model.addAttribute("currentDate", currentDateTime);
+        model.addAttribute("userName", name);
+
+        return "home"; //this string home is name of our template
+    }
+
+    //about page
+    @RequestMapping("/about")
+    public String about(){
+        System.out.println("Executing about logic");
+        return "about";
+    }
 }
--- dynamic-website/src/main/java/com/substring/dynamic/website/controllers/TestController.java
+++
@@ -1,4 +0,0 @@
-package com.substring.dynamic.website.controllers;
-
-public class TestController {
-}
